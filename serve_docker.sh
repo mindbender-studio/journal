292379755f9e1fbc4bc604132a5640fa6a6e2fc0
Marcus Ottosson <konstruktion@gmail.com>
2017-04-22 11:51:33 +0100

    Fix home button

--- serve_docker.sh
+++ serve_docker.sh
@@ -1,1 +1,10 @@
-docker run --rm -ti -v $(pwd):/src -p 4000:4000 grahamc/jekyll serve -w --force_polling -H 0.0.0.0 --port 4000
+docker run \
+	--rm \
+	-ti \
+	-v $(pwd):/src \
+	-p 4000:4000 grahamc/jekyll serve \
+	-w \
+	--baseurl /journal \
+	--force_polling \
+	-H 0.0.0.0 \
+	--port 4000
